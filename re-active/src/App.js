96ceaf90e10fde0e9605285e42cf502b22b7af2c
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-17 21:53:38 +0000

    Adding Home component for landing page

--- re-active/src/App.js
+++ re-active/src/App.js
@@ -1,11 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+
 function App() {
   return (
-    <Header />
-    // <Routes>
-    // <Route path="/" element={}/>
-    // </Routes>
+    <>
+      <Header />
+      
+      <div className="wrapper"> 
+        <Home/>
+      </div>
+      
+      <Footer/>
+    </>
+
+      // <Routes>
+      // <Route path="/" element={}/>
+      // </Routes>
   );
 }
 
