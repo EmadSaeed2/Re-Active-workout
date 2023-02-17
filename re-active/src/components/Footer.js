59e256193b3d932e75aefeecea86d971e6205769
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-17 19:37:18 +0000

    Footer component html added

---
+++ re-active/src/components/Footer.js
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer className="text-center">
+            <p>&copy 2023 ReActive Fitness</p>
+        </footer>
+    )
+  }
+  
+  export default Footer;
