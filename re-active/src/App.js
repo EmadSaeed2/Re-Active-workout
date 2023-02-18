2e289727405d20103d0dbae6e37db0e36d0de078
Emad Saeed <eaf.saeed@gmail.com>
2023-02-18 20:31:00 +0000

    init history component

--- re-active/src/App.js
+++ re-active/src/App.js
@@ -2,22 +2,20 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import History from "./components/History";
 
 function App() {
   return (
     <>
       <Header />
-      
-      <div className="wrapper"> 
-        <Home/>
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
       </div>
-      
-      <Footer/>
+      <Footer />
     </>
-
-      // <Routes>
-      // <Route path="/" element={}/>
-      // </Routes>
   );
 }
 
