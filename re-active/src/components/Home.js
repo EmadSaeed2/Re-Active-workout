8ea5b4d66dca9d30992d9052dbfaf4bcaca9ba1e
c-navarroperez <carlosnavarro.brs@gmail.com>
2023-02-22 19:23:00 +0000

    Importing exercise search component into home. Setting home status via switch case for navigation. Adjusting margins.

--- re-active/src/components/Home.js
+++ re-active/src/components/Home.js
@@ -1,15 +1,19 @@
 import FilterCategory from "./FilterCategory";
+import ExerciseSearch from "./ExerciseSearch";
 
 const homeStyle = {
   h1Style: {
     display: "flex",
     justifyContent: "center",
-    padding: "10px",
+    padding: "0px 10px 0px 10px",
+    marginBottom: "24px"
+
   },
   main: {
     borderTop: "2px solid var(--react--black)",
     display: "flex",
     height: "100%",
+    marginBottom: "24px"
   },
   excerciseStyle: {
     display: "flex",
@@ -33,17 +37,27 @@ const homeStyle = {
 
 function Home(props) {
   let { homeStatus, setHomeStatus } = props;
-  if (homeStatus === "FilterLoad") {
-    return (
-      <FilterCategory homeStatus={homeStatus} setHomeStatus={setHomeStatus} />
-    );
+  switch(homeStatus) {
+    case "loadSearch":
+      return (
+        <ExerciseSearch homeStatus={homeStatus} setHomeStatus={setHomeStatus} />
+      );
+    case "FilterLoad":
+      return (
+        <FilterCategory homeStatus={homeStatus} setHomeStatus={setHomeStatus} />
+      );
   }
+
   return (
     <>
       <h1 style={homeStyle.h1Style}>Let's Go!</h1>
       <main style={homeStyle.main}>
-        <section style={homeStyle.excerciseStyle}>
+        <section 
+          style={homeStyle.excerciseStyle}
+          onClick={() => setHomeStatus("loadSearch")}
+        >
           <h2 style={homeStyle.titleStyle}>Excercise Search</h2>
+
         </section>
         <section
           style={homeStyle.filterStyle}
