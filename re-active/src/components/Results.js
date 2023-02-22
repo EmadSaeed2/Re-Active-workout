68d1f81015bce1ec809c43df5a9e1f9cd81f848d
Kieran <108032076+BritishCryptoGuy@users.noreply.github.com>
2023-02-22 19:18:09 +0000

    added in fetch request, and customised code to take in fetch data

--- re-active/src/components/Results.js
+++ re-active/src/components/Results.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import testData from "../data/testExcerciseData.json";
 import logo from "../images/ReactDumbellSiteColours.png"; //TEMP IMAGE
+const apiKey = "tyKkR6aVa1kcx3egsUokWA==JKPcXBMyGNHKchV1";
 
 const resultsCSS = {
   main: {
@@ -12,6 +14,12 @@ const resultsCSS = {
     flexWrap: "wrap",
     padding: "10px 20px",
   },
+  h1: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "var(--react--yellow)",
+  },
   logoStyle: {
     maxWidth: "100px",
     marginRight: "10px",
@@ -20,6 +28,7 @@ const resultsCSS = {
 };
 
 function Results(props) {
+  let [results, setResults] = useState(false);
   let userSelection = props.filterCat;
   console.log(userSelection);
   function capitalise(string) {
@@ -27,34 +36,60 @@ function Results(props) {
     return firstLetter + string.slice(1);
   }
   // fetch api here using user selection and .then the main
-  return (
-    <main style={resultsCSS.main}>
-      {/* below testData to be replaced by fetch response from api  */}
-      {testData.map((data, index) => {
-        console.log(data);
-        return (
-          <div className="resultCard" key={index}>
-            <h3 className="resultData"> {data.name}</h3>
-            <p>
-              Target Muscle:
-              <span className="resultData"> {capitalise(data.muscle)}</span>
-            </p>
-            <p>
-              Equipment used:
-              <span className="resultData"> {capitalise(data.equipment)}</span>
-            </p>
-            <p>
-              Difficulty:{" "}
-              <span className="resultData">{capitalise(data.difficulty)}</span>
-            </p>
+  fetch(
+    `https://api.api-ninjas.com/v1/exercises?muscle=${userSelection.muscleType}`,
+    {
+      headers: { "X-Api-Key": apiKey },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      setResults(data);
+    })
+    .catch((err) => console.error(err));
+  if (!results) {
+    return (
+      <main style={resultsCSS.main}>
+        <h1 style={resultsCSS.h1}>Your results will load shortly!</h1>
+      </main>
+    );
+  } else {
+    return (
+      <main style={resultsCSS.main}>
+        {results.map((data, index) => {
+          return (
+            <div className="resultCard" key={index}>
+              <h3 className="resultData"> {data.name}</h3>
+              <p>
+                Target Muscle:
+                <span className="resultData"> {capitalise(data.muscle)}</span>
+              </p>
+              <p>
+                Equipment used:
+                <span className="resultData">
+                  {" "}
+                  {capitalise(data.equipment)}
+                </span>
+              </p>
+              <p>
+                Difficulty:{" "}
+                <span className="resultData">
+                  {capitalise(data.difficulty)}
+                </span>
+              </p>
 
-            <img src={logo} alt="Re-Active Logo" style={resultsCSS.logoStyle} />
-            <button className="buttonReact">Select</button>
-          </div>
-        );
-      })}
-    </main>
-  );
+              <img
+                src={logo}
+                alt="Re-Active Logo"
+                style={resultsCSS.logoStyle}
+              />
+              <button className="buttonReact">Select</button>
+            </div>
+          );
+        })}
+      </main>
+    );
+  }
 }
 
 export default Results;
